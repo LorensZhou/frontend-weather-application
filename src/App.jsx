3ed8e9d9dae31487e239b348f7941ea0d90e1dde
LorensZhou <lorenszhou@yahoo.com>
2025-05-21 18:06:26 +0200

    navigatie balk (navBar) opmaak en lay-out

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 
-import { Route, Navigate, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
 
     return (
         <>
+            <NavBar />
             <div className="content">
                 <Routes>
                     <Route path="/" element={<Home />}/>
