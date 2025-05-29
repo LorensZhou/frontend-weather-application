a8ba28a4dc112948575a74987efdce2284c12460
LorensZhou <lorenszhou@yahoo.com>
2025-05-29 22:10:36 +0200

    aanmaken helperfuncties en ophalen weergegevens voor stad detailpagina via weather api

---
+++ src/helper/kelvinToCelcius.js
@@ -0,0 +1,6 @@
+
+function kelvinToCelcius(kelvin) {
+    return Math.round(kelvin - 273.15);
+}
+
+export default kelvinToCelcius;
---
+++ src/helper/urlForWeatherIcon.js
@@ -0,0 +1,6 @@
+
+function urlForWeatherIcon(code) {
+    return `https://openweathermap.org/img/wn/${code}@2x.png`;
+}
+
+export default urlForWeatherIcon;
